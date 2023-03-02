e7442a108e22aedc4acc74673df7440d2f0fc481
Arthur Andrade <git@arthur404.dev>
2023-03-02 19:37:46 -0300

    feat: add App Component layout and style

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./app.css"
 
 const App = () => {
   const [showGame, setShowGame] = useState<boolean>(false)
-  const [repos, setRepos] = useState([])
+  const [repos, setRepos] = useState(["repo1"])
 
   const handleStartClick = () => {
     setShowGame(true)
@@ -13,12 +13,18 @@ const App = () => {
     <main>
       {!showGame && (
         <>
-          <h1>Welcome to RepoWars</h1>
-          <h2>A developer-driven guessing game</h2>
+          <h1 className='title'>Welcome to RepoWars</h1>
+          <h2 className='headline gradient text'>
+            A developer-driven guessing game
+          </h2>
           {repos.length === 0 ? (
-            <p>Loading game...</p>
+            <p className='loading'>Loading game...</p>
           ) : (
-            <button type='submit' onClick={handleStartClick}>
+            <button
+              type='submit'
+              className='button gradient'
+              onClick={handleStartClick}
+            >
               Start
             </button>
           )}
