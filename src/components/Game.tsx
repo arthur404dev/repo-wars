69b2d5e1663ff52af8de1e67a3117951a24a3d2a
Arthur Andrade <git@arthur404.dev>
2023-03-02 21:56:42 -0300

    feat: add Game logic

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { FaExchangeAlt } from "react-icons/fa"
+import { FaCheckCircle, FaExchangeAlt, FaTimesCircle } from "react-icons/fa"
 import "./game.css"
 import RepoCard from "./RepoCard"
 
@@ -14,17 +14,63 @@ const Game: React.FC<GameProps> = ({
   originalList,
   setShowGame,
 }) => {
+  const [repositories, setRepositories] = repoState
   const [over, setOver] = useState<boolean>(false)
   const [repo1, setRepo1] = useState<Repo | null>(null)
   const [repo2, setRepo2] = useState<Repo | null>(null)
-  //   TODO: Use actual random repos
+  const [correct, setCorrect] = useState<number>(0)
+  const [wrong, setWrong] = useState<number>(0)
+  // Random Repo Selection Logic
+  const randomSelect = (arr: Repo[]) => {
+    // If there are less than 2 repositories, reset the list
+    if (arr.length < 2) {
+      setRepositories(originalList)
+      setOver(true)
+      return
+    }
+    // Select two random indexes from the array
+    const generateRandom = () => Math.floor(Math.random() * arr.length)
+    const randomIndex1 = generateRandom()
+    let randomIndex2 = generateRandom()
+    // Make sure that both indexes are different
+    while (randomIndex1 === randomIndex2) {
+      randomIndex2 = generateRandom()
+    }
+    // Select the two repositories with those indexes
+    const randomRepo1 = arr[randomIndex1]
+    const randomRepo2 = arr[randomIndex2]
+    // Remove the two repositories from the array
+    const updatedArr = arr.filter(
+      (repo) => repo.id !== randomRepo1.id && repo.id !== randomRepo2.id,
+    )
+
+    // Set repos with the repo values
+
+    setRepo1(randomRepo1)
+    setRepo2(randomRepo2)
+
+    // Update the array
+    setRepositories(updatedArr)
+  }
+
+  //   Set Initial Repos
   useEffect(() => {
-    setRepo1(originalList[0])
-    setRepo2(originalList[1])
+    randomSelect(repositories)
   }, [])
 
   //   Handlers:
-  const handleChoice = (r: Repo) => {}
+  const handleChoice = (r: Repo) => {
+    const chosenRepo = r.id === repo1?.id ? repo1 : repo2
+    const winner =
+      repo1?.stargazers_count! > repo2?.stargazers_count! ? repo1 : repo2
+
+    if (chosenRepo?.id === winner?.id) {
+      setCorrect((prev) => prev + 1)
+    } else {
+      setWrong((prev) => prev + 1)
+    }
+    randomSelect(repositories)
+  }
 
   return (
     <div className='game'>
@@ -39,7 +85,18 @@ const Game: React.FC<GameProps> = ({
               <RepoCard content={repo1} handler={handleChoice} />
 
               <div className='dashboard'>
-                <FaExchangeAlt className="icon-versus" />
+                <div className='result correct'>
+                  <p>{correct}</p>
+
+                  <FaCheckCircle />
+                </div>
+
+                <FaExchangeAlt className='icon-versus' />
+
+                <div className='result wrong'>
+                  <p>{wrong}</p>
+                  <FaTimesCircle />
+                </div>
               </div>
 
               <RepoCard content={repo2} handler={handleChoice} />
@@ -61,7 +118,7 @@ const Game: React.FC<GameProps> = ({
             type='submit'
             className='button gradient'
             // TODO: change this button to refresh the current repositories
-            onClick={() => setShowGame(false)}
+            onClick={() => randomSelect(repositories)}
           >
             Skip
           </button>
